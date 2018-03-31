3dac07a4feed04b94996ec05f9e23dac86ccfe88
manthanchauhan913 <manthanchauhan913@gmail.com>
2018-03-31 19:36:32 +0530

    new function binarization added

--- crop.py
+++ crop.py
@@ -1,15 +1,13 @@
 import cv2
 import numpy as np
+from binarization import binarize
 
-input = cv2.imread('sample_image.jpg', cv2.IMREAD_GRAYSCALE)
-input = cv2.resize(input, (1366, 768))
-aftrCnny = cv2.Canny(input, 100, 200)
-# cv2.imshow('canny', aftrCnny)
+input = cv2.imread('sample_image_4.jpg', cv2.IMREAD_GRAYSCALE)
+aftrCnny = cv2.Canny(input, 100, 160)
 aftrdilat = cv2.dilate(aftrCnny, np.ones((5, 5), np.uint8), iterations=1)
 aftrdilat = cv2.dilate(aftrdilat, np.ones((5, 5), np.uint8), iterations=1)
-# cv2.imshow('dilate', aftrdilat)
 
-#counting boundary box
+#finding boundary box of crop
 im2, countours, heirarchy = cv2.findContours(aftrdilat, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 initx1 = 10e9
 inity1 = 10e9
@@ -27,16 +25,9 @@ for cntr in countours:
         initx2 = x2
     if y2 > inity2:
         inity2 = y2
-x1 = max(10, initx1 - 5)
-y1 = max(10, inity1 - 5)
-x2 = min(1366, initx2 + 5)
-y2 = min(768, inity2 + 5)
-# print(x1)
-# print(x2)
-# print(y1)
-# print(y2)
-crop = input[y1:y2, x1:x2]
-cv2.imshow('crop', crop)
+crop = input[inity1 - 5:inity2 + 5, initx1 - 5:initx2 + 5]
+output = binarize(crop)
+cv2.imshow('thresh', cv2.resize(output, (1366, 768)))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
