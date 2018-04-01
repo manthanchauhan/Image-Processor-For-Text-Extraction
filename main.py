a6cd7fd573298b315467ccf39854deb34e51dbf7
manthanchauhan913 <manthanchauhan913@gmail.com>
2018-04-01 14:03:29 +0530

    text extraction using tesseract

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+import functnDefinitions
 import cv2
-from crop import crop_It
-from binarization import binarize
 
 input = cv2.imread('sample_image.jpg', cv2.IMREAD_GRAYSCALE)
-cropped = crop_It(input)
-binary = binarize(cropped)
-cv2.imshow('output', cv2.resize(binary, (1366, 768)))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cropped = functnDefinitions.crop_It(input)
+binary = functnDefinitions.binarize(cropped)
+functnDefinitions.printToFile(binary)
+
 
