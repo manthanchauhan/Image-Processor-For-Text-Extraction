98d251c2a265355f017da514bb2d6a13654401f4
manthanchauhan913 <manthanchauhan913@gmail.com>
2018-04-04 21:03:30 +0530

    planning to use a neural net on place of tesseract ocr

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import functnDefinitions
 import cv2
 
 input = cv2.imread('sample_image.jpg', cv2.IMREAD_GRAYSCALE)
-cropped = functnDefinitions.crop_It(input)
-binary = functnDefinitions.binarize(cropped)
-functnDefinitions.printToFile(binary)
+binary = functnDefinitions.binarize(input)
+tiltCrrctd = functnDefinitions.removeTilt(binary)
+cropped = functnDefinitions.crop_It(tiltCrrctd)
+# cv2.imshow('binary', cropped)
+functnDefinitions.printToFile(cropped)
+cv2.waitKey(0)
 
 
